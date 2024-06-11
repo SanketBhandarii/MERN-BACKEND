a89f1a2834405469e81f32cfe2235d60bf04476b
SanketBhandarii <bhandarisanketp@gmail.com>
2024-06-11 23:53:24 +0530

    Updated

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import route from './routes/userRoute.js';
-
+import route from "./routes/userRoute.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
 dotenv.config();
+app.use(cors());
 
 const PORT = process.env.PORT || 7000;
 const URL = process.env.MONGOURL;
@@ -25,14 +25,14 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-   res.send("HELLO WELCOME!!")
-})
+  res.send("HELLO WELCOME!!");
+});
 app.use("/api", route);
 
 app.use((err, req, res, next) => {
-   const {status = 400 , message = "Something went wrong"} = err;
-   res.status(status).json(message);
-   next(err)
-})
+  const { status = 400, message = "Something went wrong" } = err;
+  res.status(status).json(message);
+  next(err);
+});
 
 export default app;
